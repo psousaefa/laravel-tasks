ab1fcc1f37310d8c2d55b1b8dec6ae7f547d4a94
psousaefa <126902655+psousaefa@users.noreply.github.com>
2023-03-14 09:56:25 +0000

    setup mysql config params

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'msdocs-laravel-mysql-efa-server.mysql.database.azure.com'),
+            'host' => env('DB_HOST', 'msdocs-laravel-mysql-efa-server'),
             'port' => env('DB_PORT', '3306'),
             'database'  => env('DB_DATABASE', 'msdocs-laravel-mysql-efa-database'),
             'username'  => env('DB_USERNAME', 'tiyxwiedro'),
