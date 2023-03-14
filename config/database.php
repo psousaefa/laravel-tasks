00142ee8bd632da9529ed31efc9ee289774809db
psousaefa <126902655+psousaefa@users.noreply.github.com>
2023-03-14 12:51:40 +0000

    'DB_HOST', 'msdocs-laravel-mysql-efa-server.mysql.database.azure.com'

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'msdocs-laravel-mysql-efa-server.mysql.database.azure.com'),
             'port' => env('DB_PORT', '3306'),
             'database'  => env('DB_DATABASE', 'msdocs-laravel-mysql-efa-database'),
             'username'  => env('DB_USERNAME', 'tiyxwiedro'),
